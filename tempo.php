44c2cf9c788e77c56f2e5e7005f99009fabf12ed
Thiago Rabaioli <tfrabaioli@gmail.com>
2018-05-22 21:09:56 +0100

    Add files via upload

--- tempo.php
+++ tempo.php
@@ -1,3 +1,51 @@
+<?php 
+
+    $weather = "";
+    $error = "";
+    
+    if (array_key_exists('city', $_GET)) {
+        
+        $city = str_replace(' ', '', $_GET['city']);
+        
+        $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        
+        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+    
+            $error = "Está cidade não foi encontrada.";
+
+        } else {
+        
+        $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        $pageArray = explode('3 days)</span><p class="b-forecast__table-description-content"><span class="phrase"', $forecastPage);
+            
+        if (sizeof($pageArray) > 1) {
+        
+                $secondPageArray = explode('</h2></span><p class=', $pageArray[1]);
+            
+                if (sizeof($secondPageArray) > 1) {
+
+                    $weather = $secondPageArray[0];
+                    
+                } else {
+                    
+                    $error = "Está cidade não foi encontrada.";
+                    
+                }
+            
+            } else {
+            
+                $error = "Está cidade não foi encontrada.";
+            
+            }
+        
+        }
+        
+    }
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,10 +56,89 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-    <title>Como está o tempo ? </title>
+    <title>Como está o tempo ?! </title>
+      
+      <style type="text/css">
+      
+      
+            html { 
+                    background: url(tempo.jpg) no-repeat center center fixed; 
+                    -webkit-background-size: cover;
+                    -moz-background-size: cover;
+                    -o-background-size: cover;
+                    background-size: cover;
+                }
+          
+          body{
+              background: none;
+          }
+          
+          .container{
+              text-align: center;
+              margin-top: 200px;
+              width: 450px;
+          }
+          input{
+              margin: 20px 0;
+          }
+          
+          #weather{
+              margin-top: 12px;
+          }
+          
+      </style>
+      
+      
   </head>
+    
+  
   <body>
-    <h1>Hello, world!</h1>
+      
+      <div class="container">
+      
+      <h1> Como está o tempo ?! </h1>
+          
+          <form method="get">
+                <div class="form-group">
+                <label for="city"></label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Exemplo: London, Tokyo" value="<?php 
+																										   
+																										   if (array_key_exists('city', $_GET)) {
+																										   
+																										   echo $_GET['city']; 
+																										   
+																										   }
+																										   
+																										   ?>">
+               
+                </div>
+                  
+                  <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          
+          
+           <div id="weather"><?php 
+              
+              if ($weather) {
+                  
+                  echo '<div class="alert alert-success" role="alert">
+  '.$weather.'
+</div>';
+                  
+              } else if ($error) {
+                  
+                  echo '<div class="alert alert-danger" role="alert">
+  '.$error.'
+</div>';
+                  
+              }
+              
+              ?></div>
+      
+      </div>
+   
+      
+      
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
